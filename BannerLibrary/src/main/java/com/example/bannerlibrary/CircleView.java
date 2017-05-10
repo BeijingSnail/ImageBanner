f40e8a8fd2d1a5e0e9c1910529ac1a0644ac4ff0
BeijingSnail <m15142555427@163.com>
2017-05-10 16:34:13 +0800

    Sorting folder

---
+++ BannerLibrary/src/main/java/com/example/bannerlibrary/CircleView.java
@@ -0,0 +1,51 @@
+package com.example.bannerlibrary;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 自定义圆形view（小圆点）
+ * Created by zhangzhiqiang on 2017/4/28.
+ */
+
+public class CircleView extends View {
+
+    private Paint circlePaint;
+
+    public CircleView(Context context) {
+        this(context, null);
+    }
+
+    public CircleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        this.circlePaint = new Paint();
+        this.circlePaint.setAntiAlias(true);//消除锯齿
+        this.circlePaint.setStyle(Paint.Style.FILL);
+
+    }
+
+    public void setCircleColor(int resource) {
+        //设置圆点颜色
+        circlePaint.setColor(resource);
+        //刷新onDraw
+        invalidate();
+        
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int center = getWidth() / 2;
+        //以view的1/2作为圆心画圆
+        canvas.drawCircle(center, center, center, circlePaint);
+
+    }
+
+}
